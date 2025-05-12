8b8969d93cdd361386757ce71a8dc0d0a7060d3a
Filip Biedrzycki <fbiedrzycki@psi.pl>
2025-05-12 11:25:41 +0200

    Exposing TreeCellSkin's behavior creator

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/TreeCellSkin.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/TreeCellSkin.java
@@ -110,7 +110,7 @@ public class TreeCellSkin<T> extends CellSkinBase<TreeCell<T>> {
         super(control);
 
         // install default input map for the TreeCell control
-        behavior = new TreeCellBehavior<>(control);
+        behavior = createBehavior(control);
 //        control.setInputMap(behavior.getInputMap());
 
         updateTreeItem();
@@ -123,6 +123,10 @@ public class TreeCellSkin<T> extends CellSkinBase<TreeCell<T>> {
         registerChangeListener(control.textProperty(), e -> getSkinnable().requestLayout());
     }
 
+    protected TreeCellBehavior< T > createBehavior( final TreeCell< T > control )
+    {
+        return new TreeCellBehavior<>(control);
+    }
 
     /* *************************************************************************
      *                                                                         *
