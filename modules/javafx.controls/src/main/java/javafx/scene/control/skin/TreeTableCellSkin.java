9f6145fb48c8565182b6d3d5267d18404863f278
Filip Biedrzycki <fbiedrzycki@psi.pl>
2025-05-19 16:04:31 +0200

    Exposing calculateIndentation in TreeTableCellSkin

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/TreeTableCellSkin.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/TreeTableCellSkin.java
@@ -138,7 +138,7 @@ public class TreeTableCellSkin<S,T> extends TableCellSkinBase<TreeItem<S>, T, Tr
         return super.computePrefHeight(width, topInset, rightInset, bottomInset, leftInset);
     }
 
-    private double calculateIndentation() {
+    protected double calculateIndentation() {
         double indentation = 0;
 
         // RT-27167: we must take into account the disclosure node and the
